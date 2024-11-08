6e7b3506216ffdcf8e62697e587b838f0d683631
Bazuka <bazukaffpedro@gmail.com>
2024-11-07 23:19:47 -0300

    Explicando tipos de comentarios

--- Aulas/aula1.py
+++ Aulas/aula1.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+# O hashtag permite a escrita de um comentario
+#Acima
+print(123) # Na frente
+#Abaixo
+
+"""
+
+As três aspas juntas significam o DocString
+Isso é lido pelo interpretador do python mas é ignorado como se fosse um comentario, mas não é
+
+
+Pode ser utilizado como comentario de mais de 1 linha
+"""
+
+'''
+
+O DocString também permite a utilizção das aspas simples
+
+'''
