89c248f020a6eb238f62633e83e8e9a1372e8200
Bazuka <bazukaffpedro@gmail.com>
2024-11-10 01:18:36 -0300

    Adicionando mais coisas na aula2

---
+++ Aulas/aula2.py
@@ -0,0 +1,37 @@
+# Serve para printar alguma coisa de acordo com o argumento descrito entre os parenteses
+print(423, 86)
+
+#O python automaticamente faz a quebra de linha quando exibe os resultados
+print(423, 86)
+print(423, 86)
+
+'''
+
+Comando "sep" no python
+Serve para criar um separador entre 2 ou mais argumentos
+Sua sintaxe é:
+
+'''
+
+sep=""
+
+'''
+onde dentro dos parenteses pode incluir espaço ou qualquer tipo de caracter que deseja colocar entre os dois argumentos
+
+p.ex:
+
+'''
+
+print(423, 86, sep="-+-")
+
+print(423, 86, sep=" = ")
+
+'''
+OBS: Os argumentos precisam estar juntos um do outro 
+
+Exemplo de quando o comando sep não funciona:
+
+'''
+print("Este numero {} esta separado deste {} por essa frase".format(423, 23, sep=" = "))
+
+# Como visto o numero acima não fez a separação com o (" = ")
