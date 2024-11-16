924c2b484afee8720fd6292b01b3ca137c0b5a25
Bazuka <bazukaffpedro@gmail.com>
2024-11-16 17:47:21 -0300

    Reparando detalher da aula3

--- Aulas/aula3.py
+++ Aulas/aula3.py
@@ -1,11 +1,14 @@
 """
 
+DocString """ """
 Python = Linguagem de programação
 Tipo de tipagem = Dinamica / Forte
 str -> string -> texto
 Strings são textos que estão dentro de aspas
 
 """
+
+# Textos entre aspas simples e aspas duplas no python, não fazem diferença
 
 # Aspas simples
 
@@ -17,6 +20,16 @@ print("Aspas duplas")
 
 # Escape
 
+# O "Escape" serve para anular algo que podeia ser considerado como codigo dentro das aspas.
+
+#P.ex:
+
 print("É assim como o \"escape\" funciona")
 
+#O "Escape" é utilizado com a barra invertida (\) e pode ser colocado depois das apas para o interpretador não considerar como fim do texto da primeira aspa
+
 # r
+
+# Exibe o caractere de Escape
+
+print("É assim como o \"escape\" funciona")
