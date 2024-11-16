961e339dc7b9c5ba9bc925fb27ad823f2148752e
Bazuka <bazukaffpedro@gmail.com>
2024-11-16 18:20:48 -0300

    Corrigindo erros de save

---
+++ Aulas/aula5.py
@@ -0,0 +1,8 @@
+# Tipo de dado bool (booleano)
+# Ao questionar algo em um programa, só existem duas respostas possiveis:
+# sim (True) ou não (False)
+# Existem varios operadores para "Questionar".
+# Dentre eles o ==, que é um operador lógico que questiona se um valor é igual a outro
+#
+#
+#
