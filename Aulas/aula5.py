23a196af171bc40478721fc2fcb8a956c3527c30
Bazuka <bazukastream8866@gmail.com>
2024-11-16 18:47:26 -0300

    Adicionando mais informações na aula5

--- Aulas/aula5.py
+++ Aulas/aula5.py
@@ -5,4 +5,10 @@
 # Dentre eles o ==, que é um operador lógico que questiona se um valor é igual a outro
 #
 #
-#
+print(10 == 10) # Sim -> True (Verdadeiro) 
+print(20 ==3)   # Não -> False (Falso)
+
+print(type(True))
+print(type(False))
+print(type(10 == 10))
+print(type(20 ==3))
