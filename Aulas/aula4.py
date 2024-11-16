56eea319225c4f08f9379bddf0663409c462b74e
Bazuka <bazukaffpedro@gmail.com>
2024-11-16 18:12:19 -0300

    Adicionando informações na aula4

---
+++ Aulas/aula4.py
@@ -0,0 +1,19 @@
+# Tipos int e float
+# O tipo int representa qualquer número positivo ou negativo. int sem sinal é considerado positivo
+print(12)
+print(-13)
+print(0)
+
+# float -> Número com ponto é um numero flutuante
+# Ou seja número flutuante é um numero racional
+# positivo ou negativo com ponto flutuante
+# float sem sinal é considerado como positivo
+print(1.2, 12.3)
+print(0.0, -1.6)
+
+# A função type mostra o tipo da função no Python
+# interferiu o valor
+
+print(type('Pedro'))
+print(type(4))
+print(type(4.6), type(-4.6), type(0.0))
