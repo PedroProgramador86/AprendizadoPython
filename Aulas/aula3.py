4a4f0aaa9895439571411a5e7ebbf600e9fe4e74
Bazuka <bazukaffpedro@gmail.com>
2024-11-15 23:38:00 -0300

    Adicionando umas informações sobre a aula 3 e 4

---
+++ Aulas/aula3.py
@@ -0,0 +1,22 @@
+"""
+
+Python = Linguagem de programação
+Tipo de tipagem = Dinamica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+
+"""
+
+# Aspas simples
+
+print('Aspas simples')
+
+# Aspas duplas
+
+print("Aspas duplas")
+
+# Escape
+
+print("É assim como o \"escape\" funciona")
+
+# r
