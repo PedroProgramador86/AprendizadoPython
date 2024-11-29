fb2af72234155d0e01ad3d6b771c582b1b11c3c7
Bazuka <bazukastream8866@gmail.com>
2024-11-29 19:32:49 -0300

    Adicionando aula8

---
+++ Aulas/aula8.py
@@ -0,0 +1,14 @@
+
+nome = 'Pedro Enzo'
+sobrenome = 'Lima Luna'
+idade = 18
+ano_de_nascimento = 2024 - 18
+maiordeidade = idade >= 18
+altura = 1.80
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de Nascimento: ", ano_de_nascimento)
+print("É maior de idade ? ", maiordeidade)
+print("Altura em metros: ", altura)
