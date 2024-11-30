dbb6f692abb8510f7edc714ab85eafde185ae767
Bazuka <bazukastream8866@gmail.com>
2024-11-29 23:11:06 -0300

    Adicionando aula9

---
+++ Aulas/aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print("Adicao ", adicao)
+
+subtracao = 10 - 5
+print("Subtracao ", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicaçao ", multiplicacao)
+
+divisao = 10 / 2
+print("Divisão ", divisao)
+
+divisao_inteira = 10 // 2.2
+print("Divisão inteira ", divisao_inteira)
+
+expotenciacao = 2 ** 10
+print("Expotenciação: ", expotenciacao)
+
+modulo = 55 % 5
+print("Modulo: ", modulo)
