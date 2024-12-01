88317b931dc227013dee82f13ce9403c36b8f5c3
Bazuka <bazukastream8866@gmail.com>
2024-12-01 19:08:36 -0300

    Adicionando aula10

---
+++ Aulas/aula10.py
@@ -0,0 +1,9 @@
+concatenacao = 'A ' + 'B ' + 'C '
+concatenacao2 = 'Pedro' + str(1) + ' Enzo'
+print(concatenacao)
+print(concatenacao2)
+
+a_dez_vezes = 'A ' * 10
+tres_vezes_luiz = 3 * 'Luiz '
+print(a_dez_vezes)
+print(tres_vezes_luiz)
