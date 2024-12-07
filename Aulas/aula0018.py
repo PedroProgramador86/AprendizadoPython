aa9015e1410613a122abaeac330f105e97ffe009
Bazuka <bazukastream8866@gmail.com>
2024-12-07 13:13:23 -0300

    Adicionando aula0018

---
+++ Aulas/aula0018.py
@@ -0,0 +1,52 @@
+# if  /   elif    / else
+# se / Se não se / se não
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print("Código para condição 1")
+elif condicao2:
+    print("Código para condição 2")
+elif condicao3:
+    print("Código para condição 3")
+elif condicao4:
+    print("Código para condição 4")
+else:
+    print("Nenhuma condição foi satisfeita")
+
+
+
+if condicao1:
+    print('Este é um codigo do if')
+
+print("Comando fora do if")
+
+# Condiçao falsa
+
+condicao = False
+
+if condicao2:
+    print('Este é um codigo do if')
+
+else:
+    print("Este é o else do primeiro if")
+
+if 10 == 10:
+    print("Outro if")
+
+print("Comando fora do if")
+
+
+vf = input("Digite True ou False: ")
+
+if vf == "True":
+    print("Essa condição é verdadeira")
+
+elif vf == "False":
+    print("Essa condição é falsa")
+
+else:
+    print("Nenhuma das respostas é satisfatoria...")
