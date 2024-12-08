d05c0731b483ccda406fb1ecc8ba505cd68554a8
BashScript <bashscript86@gmail.com>
2024-12-07 23:09:27 -0300

    corrigindo commit

---
+++ Aulas/aula0019.py
@@ -0,0 +1,12 @@
+"""
+
+Operadores de comparação (relacionais)
+OP          Significado             Exemplo(True)
+>           Maior                   2 > 1
+>=          Maior ou igual          2 <= 2
+<           menor                   1 < 2
+<=          menor ou igual          2 <= 2
+==          igual                   'a' == 'a'
+!=          diferente               'a' != 'b'
+
+"""
