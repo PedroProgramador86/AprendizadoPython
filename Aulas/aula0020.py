5656e81661b54604b2145a8b701d5b9429b23f31
Bazuka <bazukastream8866@gmail.com>
2024-12-11 19:52:14 -0300

    Adicionando aula0020

---
+++ Aulas/aula0020.py
@@ -0,0 +1,49 @@
+# valor1 = input("Digite um valor: ")
+# valor2 = input("Digite outro valor: ")
+
+# int_valor1 = int(valor1)
+# int_valor2 = int(valor2)
+
+# if(int_valor1 > int_valor2):
+#     print(f"O primeiro valor {int_valor1} é maior que o segundo ({int_valor2})")
+
+# else:
+#     print(f"O segundo valor {int_valor2} é maior que o primeiro valor ({int_valor1})")
+
+# # 
+# # 
+# # 
+
+# valor1 = input("Digite um valor: ")
+# valor2 = input("Digite outro valor: ")
+
+# int_valor1 = int(valor1)
+# int_valor2 = int(valor2)
+
+# if(int_valor1 > int_valor2):
+#     print(f"O primeiro valor {valor1} é maior que o segundo ({valor2})")
+
+# else:
+#     print(f"O segundo valor {valor2} é maior que o primeiro valor ({valor1})")
+
+# 
+#
+#
+
+# v1 = input("Digite um valor: ")
+# v2 = input("Digite outro valor: ")
+
+# if v1 > v2:
+#     print("O primeiro valor é maior que o segundo")
+
+# else:
+#     print("O segundo valor é maior que o primeiro")
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite um outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor=} é maior do que {segundo_valor=}')
+
+else:
+    print(f'{segundo_valor=} é maior do que {primeiro_valor=}')
