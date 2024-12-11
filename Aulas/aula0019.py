56230e57a37f32bf52c383902673b46f6b351cb0
Bazuka <bazukastream8866@gmail.com>
2024-12-11 19:09:41 -0300

    Corrigindo commit

--- Aulas/aula0019.py
+++ Aulas/aula0019.py
@@ -10,3 +10,11 @@ OP          Significado             Exemplo(True)
 !=          diferente               'a' != 'b'
 
 """
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 3 == 3
+diferente = 'a' != 'b'
+
