7ca55c873523e169b4e03919f34474ece7da6182
Bazuka <bazukastream8866@gmail.com>
2024-12-12 10:36:15 -0300

    Adicionando aula0023

---
+++ Aulas/aula0023.py
@@ -0,0 +1,16 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+
+# if not senha:
+
+#     print("Você não digitou a senha !")
+
+print(not True)
+print(not False)
+print(not 0)
+print(not 1)
+
