039e548badd07ad0eff5b4d2dcac16c863f8fcfa
Bazuka <bazukastream8866@gmail.com>
2024-12-30 10:13:53 -0300

    Adicionando aula0033

---
+++ Aulas/aula0033.py
@@ -0,0 +1,17 @@
+"""
+
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutveis que vimos: str, int, float, bool
+
+"""
+
+# Não é possivel fazer alterações por que o dado é imutavel:
+
+#p.ex:
+
+
+string = 'Pedro Enzo'
+# outra_variavel = string
+
+string[3] = 'ABC'
+print(string[4])
